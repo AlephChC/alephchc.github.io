d8594f783f0d7d794453864e8676f7d49572eb00
AlephChC <alephchc@gmail.com>
2024-03-06 22:38:29 +0800

    Site updated: 2024-03-06 22:38:28

--- js/custom-1.js
+++ js/custom-1.js
@@ -1,36 +1,39 @@
 // var a_idx = 0;
 jQuery(document).ready(function($) {
     // var time = $.timeStamp;
+    var isClick = false;
     $("html").click(function(e) {
         // var timego = e.timeStamp - time;
         // if(timego > 1536){
-
- 
-
-        var randomColor = '#'+ ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6);
-        var a = new Array
-        ("通", "江", "止", "遇", "蟹", "臻", "山", "效", "果", "假", "宕", "梗", "曾", "流", "深", "咸");
-        var $i = $("<span/>").text(a[Math.floor(Math.random() * a.length)]);
-        // a_idx = (a_idx + 1) % a.length;
-        var x = e.pageX,
-        y = e.pageY;
-        $i.css({
-            "z-index": 8,
-            "top": y - 20,
-            "left": x,
-            "position": "absolute",
-            "font-weight": "bold",
-            "color": randomColor
-        });
-        $("html").append($i);
-        $i.animate({
-            "top": y - 200,
-            "opacity": 0
-        },
-            2000,
-            function() {
+            if (isClick) {
+                isClick = false;
+                return false;
+            }
+            var randomColor = '#'+ ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6);
+            var a = new Array
+            ("通", "江", "止", "遇", "蟹", "臻", "山", "效", "果", "假", "宕", "梗", "曾", "流", "深", "咸");
+            var $i = $("<span/>").text(a[Math.floor(Math.random() * a.length)]);
+         // a_idx = (a_idx + 1) % a.length;
+            var x = e.pageX,
+            y = e.pageY;
+            $i.css({
+                "z-index": 8,
+                "top": y - 20,
+                "left": x,
+                "position": "absolute",
+                "font-weight": "bold",
+                "color": randomColor
+                  });
+             $("html").append($i);
+             console.log("Sample log");
+             $i.animate({
+                "top": y - 200,
+                "opacity": 0
+            },2000, function() {
                 $i.css('display', 'none');
             });
+
+            isClick = true;
 
  // if (time == 0) {
 
@@ -55,7 +58,7 @@ jQuery(document).ready(function($) {
  //    }
 
 
-
+// };
 // };
 
 // time = e.timeStamp;
